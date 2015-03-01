3db35206c02cb4c7931f859911c007b06964315e
Matteo Gobbi <job@matteogobbi.it>
2015-03-01 21:19:48 +0000

    Created MGSpotyView

---
+++ MGSpotyViewController/MGSpotyView.h
@@ -0,0 +1,13 @@
+//
+//  MGSpotyView.h
+//  MGSpotyView
+//
+//  Created by Matteo Gobbi on 01/03/2015.
+//  Copyright (c) 2015 Matteo Gobbi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MGSpotyView : UIView
+
+@end
---
+++ MGSpotyViewController/MGSpotyView.m
@@ -0,0 +1,13 @@
+//
+//  MGSpotyView.m
+//  MGSpotyView
+//
+//  Created by Matteo Gobbi on 01/03/2015.
+//  Copyright (c) 2015 Matteo Gobbi. All rights reserved.
+//
+
+#import "MGSpotyView.h"
+
+@implementation MGSpotyView
+
+@end
