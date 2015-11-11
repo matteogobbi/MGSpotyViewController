20d9c45b860b0854d8ca1501e4740c7daa83356b
Matteo Gobbi <job@matteogobbi.it>
2015-11-11 15:30:23 +0000

    check if exist a subview under the tapped point, which has targets or gestureRecognizer

--- MGSpotyViewController/MGSpotyView.m
+++ MGSpotyViewController/MGSpotyView.m
@@ -18,10 +18,54 @@
     
     if (CGRectContainsPoint(rect, point)) {
         CGPoint convertedPoint = [_overview convertPoint:point fromView:self];
-        return [_overview hitTest:convertedPoint withEvent:event];;
+
+        if ([self bsu_existActionsOnSubviewsOfView:_overview underPoint:convertedPoint]) {
+            return [_overview hitTest:convertedPoint withEvent:event];
+        }
     }
     
     return [super hitTest:point withEvent:event];
+}
+
+- (BOOL)bsu_existActionsOnSubviewsOfView:(__kindof UIView *)mainView underPoint:(CGPoint)point
+{
+    for (id subview in mainView.subviews) {
+        if ([self bsu_deepExistActionsOnSubviewsOfView:subview underPoint:point]) {
+            return YES;
+        }
+    }
+    
+    return NO;
+}
+
+- (BOOL)bsu_deepExistActionsOnSubviewsOfView:(__kindof UIView *)mainView underPoint:(CGPoint)point
+{
+    UIView *view = mainView;
+    
+    if (!CGRectContainsPoint(view.bounds, point)) {
+        return NO;
+    }
+    
+    if (view.gestureRecognizers.count > 0) {
+        return YES;
+    }
+    
+    if ([mainView isKindOfClass:[UIControl class]]) {
+        UIControl *control = mainView;
+        if (control.allTargets > 0) {
+            return YES;
+        }
+    }
+    
+    // Check if is in a subview of the _overview
+    for (id subview in view.subviews) {
+        CGPoint newPoint = [subview convertPoint:point fromView:mainView];
+        if ([self bsu_deepExistActionsOnSubviewsOfView:subview underPoint:newPoint]) {
+            return YES;
+        }
+    }
+    
+    return NO;
 }
 
 @end
